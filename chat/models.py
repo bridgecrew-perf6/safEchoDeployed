5d0bc5676a4119f33010f182a5c771a7dc59940d
Sadeeed <sadeeedw@gmail.com>
2021-10-10 19:13:42 +0500

    :package: add Chat and Chat Content Model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from accounts.models import Profile
 
-# Create your models here.
+
+class ChatContent(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    text = models.CharField(max_length=256, unique=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Chat(models.Model):
+    chat_name = models.CharField(blank=True, null=True, max_length=256, unique=True)
+    chat_content = models.ManyToManyField(ChatContent)
