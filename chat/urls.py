d815ac88e9a92b7bc90db734226af1cda98cef82
j <jamshaid1824@gmail.com>
2021-10-21 18:59:46 +0500

    bot integration

--- chat/urls.py
+++ chat/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('chat', views.ChatView.as_view(), name='chat'),
     path('get_chat_content/<int:pk>/', views.GetChatContentView.as_view(), name='get_chat_content'),
     path('get_chat_list/', views.GetChatListView.as_view(), name='get_chat_list'),
+    path('send_message/<int:coversation_id>/', views.SendMessageView.as_view(), name='send_message'),
 ]
