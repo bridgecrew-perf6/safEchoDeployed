3e8e146d9ea0fd22d0ad92bc73b3962cc8af7f1a
Tahir Fazal <tahirawan4@gmail.com>
2021-11-17 14:49:08 +0500

    Index setup.

---
+++ Project/search_indexes.py
@@ -0,0 +1,16 @@
+from data.models import ScrapedContent
+from haystack import indexes
+from datetime import datetime
+
+
+class ContentIndex(indexes.SearchIndex, indexes.Indexable):
+    text = indexes.CharField(document=True, use_template=True)
+    heading = indexes.CharField(model_attr='heading')
+    paragraph = indexes.DateTimeField(model_attr='paragraph')
+
+    def get_model(self):
+        return ScrapedContent
+
+    def index_queryset(self, using=None):
+        """Used when the entire index for model is updated."""
+        return self.get_model().objects.filter(created_at__lte=datetime.now())
